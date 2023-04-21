bd2ff4ddfeef6e09224c6d0291a1aa0daafdea99
Devil Ajay <inconducive@gmail.com>
2023-04-22 03:02:11 +0530

    curl cli puller done, go lib WIP

--- m.go
+++ m.go
@@ -57,7 +57,6 @@ curl --request GET \
 tested_working_uri
 curl --request GET --url "https://eth-mainnet.g.alchemy.com/nft/v2/WtwzAuh31WD6CR1u8p9sMv46ShxLJKuw/getFloorPrice?contractAddress=0xe785E82358879F061BC3dcAC6f0444462D4b5330" --header "accept: application/json"
 
-// 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
 */
 
 func main() {
